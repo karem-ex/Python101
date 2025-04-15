08a097daddc8acb62101b1000b4ae690ee34d700
Karem C <keremcif19@gmail.com>
2025-04-15 19:41:08 +0300

    Create Numeric_Types.py

---
+++ Chapter 3/Numeric_Types.py
@@ -0,0 +1,20 @@
+# integner number
+myint_ = 15
+my_int2 = int("150")
+
+# float number
+my_float = 55.0
+myfloat = float("15.25")
+
+# complex number
+my_complex = 1 + 2j
+
+# comp.real real attribute
+
+# comp.imag imag attribute
+
+complex(5, 55)
+# (5+55j)
+
+
+
